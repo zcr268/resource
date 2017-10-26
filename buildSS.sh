6b033d766059a01055880a04ecbf8f00bbca7fff
赵超冉 <zhaochaoran@weidian.com>
2017-10-26 17:48:00 +0800

    buildSS.sh

---
+++ buildSS.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+sudo -H pip install shadowsocks
+read -p "Enter your password : " password 
+sudo ssserver -p 8000 -k $password -m rc4-md5 -d start
+echo "rc4-md5 password : $password"
