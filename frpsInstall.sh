97061c3c96647ea56132d175c9e87246ab74c881
zcr268 <zcr268@sina.com>
2018-01-19 18:02:28 +0800

    Create frpsInstall.sh

---
+++ frpsInstall.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate https://raw.githubusercontent.com/clangcn/onekey-install-shell/master/frps/install-frps.sh -O ./install-frps.sh
+chmod 700 ./install-frps.sh
+./install-frps.sh install
