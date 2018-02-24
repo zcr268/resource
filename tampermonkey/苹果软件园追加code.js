7784d5dc22f6f0f0bb84d17f9666a563f6270aac
zcr268 <zcr268@sina.com>
2018-02-25 02:31:26 +0800

    Update 苹果软件园追加code.js

--- tampermonkey/苹果软件园追加code.js
+++ tampermonkey/苹果软件园追加code.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         苹果软件园追加code
 // @namespace    https://github.com/zcr268/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       LIONST
 // @include      *://www.maczapp.com/*
@@ -13,7 +13,7 @@
     'use strict';
     $('dl.file_list tr').each(function() {
         if ($(this).find('span.rounded.staus_green')[0] && $(this).find('span.rounded.staus_green')[0].innerHTML.indexOf("访问密码:") != -1) {
-            code = $(this).find('span.rounded.staus_green')[0].innerHTML.split(":")[1];
+            var code = $(this).find('span.rounded.staus_green')[0].innerHTML.split(":")[1];
             $(this).find('a').each(function() {
                 this.href = this.href + "#" + code;
             });
