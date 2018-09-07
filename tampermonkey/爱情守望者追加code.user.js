dc67e2b55c57391eec858323826abcbdf252d0fc
zcr268 <zcr268@sina.com>
2018-09-07 17:37:57 +0800

    Update 爱情守望者追加code.user.js

--- tampermonkey/爱情守望者追加code.user.js
+++ tampermonkey/爱情守望者追加code.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         爱情守望者追加code
 // @namespace    https://github.com/zcr268/resource/tree/master/tampermonkey/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @include      *://www.waitsun.com*
@@ -9,7 +9,7 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    var classsType="btn btn-donate mr-3 downtip";
+    var classsType="wu-dropdown wu-baidu";
     if (/^https:\/\/www.waitsun.com\//.test(location.href)) {
         console.log("www.waitsun.com 运行");
         var down = document.getElementsByClassName(classsType);
