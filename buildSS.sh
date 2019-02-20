3012b2ecd2722b05fcadb36edfaee89e36124ad7
zcr268 <zcr268@sina.com>
2019-02-20 17:35:11 +0800

    Update buildSS.sh

--- buildSS.sh
+++ buildSS.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # wget https://raw.githubusercontent.com/zcr268/resource/master/buildSS.sh && chmod u+x buildSS.sh && ./buildSS.sh
 sudo apt-get update
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 get-pip.py
+sudo apt -y install python-pip
 sudo -H pip install shadowsocks
 read -p "Enter your password : " password 
 sudo ssserver -p 8000 -k $password -m rc4-md5 -d start
