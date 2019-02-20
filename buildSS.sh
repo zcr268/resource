daf21e49ce86e880862c0efa2646136fe7685ac6
zcr268 <zcr268@sina.com>
2019-02-20 19:02:04 +0800

    Update buildSS.sh

--- buildSS.sh
+++ buildSS.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # wget https://raw.githubusercontent.com/zcr268/resource/master/buildSS.sh && chmod u+x buildSS.sh && ./buildSS.sh
 sudo apt-get update
-sudo apt-get -y install python-pip python-setuptools m2crypto
-
-sudo -H pip install shadowsocks
+sudo apt-get -y install shadowsocks
 read -p "Enter your password : " password 
 sudo ssserver -p 8000 -k $password -m rc4-md5 -d start
 echo "rc4-md5 password : $password"
